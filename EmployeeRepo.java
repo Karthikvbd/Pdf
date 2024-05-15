b83bf5a9471afbcac653770d078e8aed44a34bd4
Karthikvbd <147474524+Karthikvbd@users.noreply.github.com>
2024-05-15 21:51:06 +0530

    Delete EmployeeRepo.java

--- EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.mamce.EmployeeService.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employees, Integer>{
-
-	
-}
